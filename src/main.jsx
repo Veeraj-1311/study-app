844332e8af02dabae3175f7044d3b369fbd7671e
Veeraj-1311 <vinay@stage.in>
2026-05-18 13:31:37 +0530

    Remove Firebase auth requirement

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './contexts/ThemeProvider.jsx'
-import { AuthProvider } from './contexts/AuthProvider.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </AuthProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
 )
